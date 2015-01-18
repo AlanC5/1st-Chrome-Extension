d6a289c48728887c05ad981bc91a8ac048d6260e
Alan Chen <alanc1995@gmail.com>
2015-01-18 18:06:46 -0500

    Successfully stores urls and visitcounts for a 24 hour period.

--- read.js
+++ read.js
@@ -1,11 +1,3 @@
-function onClick(event) {
-  chrome.tabs.create({
-    selected: true,
-    url: 'http://www.google.com'
-  });
-  return false;
-}
-
 function addElements(divName, data) {
 	var element = document.getElementById(divName);
 
@@ -15,15 +7,37 @@ function addElements(divName, data) {
 	for (var i = 0, lens = data.length; i < lens; ++i) {
 		var d = document.createElement('d');
 		d.appendChild(document.createTextNode(data[i]));
-		d.addEventListener('click', onClick);
+		
 		var li = document.createElement('li');
 		li.appendChild(d);
 		ol.appendChild(li);
 	}
 }
 
-var House = ["Statue", "People", "Fireplace"];
+//Search up to 100 links, counts them, and shows them in a popup
+function buildHistoryList() {
+	//Look for history items in the last 24 hours
+	var microsecondsPerDay = 1000 * 60 * 60 * 24;
+	var oneDayAgo = (new Date()).getTime() - microsecondsPerDay;
+	
+	var historyStore = [];
+
+	var historiesprocessed = 0;
+	var histories = [];
+
+	chrome.history.search({text:'', maxResults:0, startTime: oneDayAgo}, function(historyItems) {
+		for (var i = 0; i < historyItems.length; i++) {
+			histories.push({
+				url: historyItems[i].url,
+				visits: historyItems[i].visitCount
+			});
+			console.log(historyItems[i].url);
+		}
+	});
+}
+
+
 
 document.addEventListener("DOMContentLoaded", function() {
-	addElements('Chart', House);
+	buildHistoryList();
 });
