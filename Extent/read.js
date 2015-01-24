3f4d08a81082cf3bbe130a17846c68b0c42636ff
Alan Chen <alanc1995@gmail.com>
2015-01-24 18:48:37 -0500

    Working on Errors

---
+++ Extent/read.js
@@ -0,0 +1,59 @@
+/* Implement Later
+function addElements(divName, data) {
+	var element = document.getElementById(divName);
+
+	var ol = document.createElement('ol');
+	element.appendChild(ol);
+
+	for (var i = 0, lens = data.length; i < lens; ++i) {
+		var d = document.createElement('d');
+		d.appendChild(document.createTextNode(data[i]));
+		
+		var li = document.createElement('li');
+		li.appendChild(d);
+		ol.appendChild(li);
+	}
+} */
+
+
+
+
+
+//Search up to 100 links, counts them, and shows them in a popup
+function buildHistoryList() {				//REMINDER: ADD BACK ELEMENT PARAMETER
+	//Look for history items in the last 24 hours
+	var microsecondsPerDay = 1000 * 60 * 60 * 24;
+	var oneDayAgo = (new Date()).getTime() - microsecondsPerDay;
+	//Array that stores url and visitCounts
+	var histories = [];
+
+	chrome.history.search({
+		text:'',				//Captures all links
+		maxResults:1,			//No max
+		startTime: oneDayAgo
+		}, function(historyItems) {
+		for (var i = 0; i < historyItems.length; i++) {
+			histories.push({
+				url: historyItems[i].url,
+				visits: historyItems[i].visitCount
+			});
+			console.log(historyItems[i].url);
+			//console.log(historyItems[i].visitCount);
+			//Send information to port using AJAX and JQUERY
+			$.ajax ({
+				type: "POST",
+				url: "http://localhost:7000",
+				crossDomain: true,
+				dataType: "json",
+				data:JSON.stringify(historyItems[i].url)
+			}).done(function (data) {alert("FINISHED");
+				});
+		}
+	});
+}
+
+
+
+document.addEventListener("DOMContentLoaded", function() {
+	buildHistoryList();
+});
---
+++ Server.js
@@ -0,0 +1,152 @@
+//RUN USING NODE
+
+
+var mysql = require('mysql');
+var client = mysql.createConnection({
+	host		: 'localhost',
+	user		: 'Alan',
+	password	: '12345'
+});
+/*Testing if mysql can take arrays, it works
+var string = ['jack', 'apples', "pillow", "Couch", "house"];
+var num = [23,5,2,20,8];
+*/
+
+check_Error = function(type, error) {
+	if (error) {
+		console.log(type + ' ERROR: ' +  error.message);
+		throw error;
+	}
+	return;
+};
+
+console.log('Connecting to Database ...');
+
+client.connect(function(err, results) {
+	check_Error('Connect', err);
+	console.log('Connected!');
+	createDB(client);
+});
+
+createDB = function (client) {
+	client.query('CREATE DATABASE Mobius', function(err, results) {
+		if (err && err.number != mysql.ERROR_DB_CREATE_EXISTS) {
+			console.log('createDB' + err.message);
+			throw err;
+		}
+		console.log('Database is created or already exists.');
+		dbExists(client);
+	});
+};
+
+dbExists = function (client) {
+	client.query('USE Mobius', function(err, results) {
+		check_Error('dbExists', err);
+		createTable(client);
+	});
+};
+
+createTable = function (client) {
+	client.query(
+		'CREATE TABLE History' +		//Create Table and names it
+		'(ID INT(11) AUTO_INCREMENT, ' +	//Specify the column and types in the rows below
+		'URL TEXT,	' +
+		'Visits INT(11),	' +
+		'PRIMARY KEY (ID));', function (err,results) {
+			if (err && err.number != mysql.ERROR_TABLE_EXISTS_ERROR) {
+				console.log('createTable ERROR: ' + err.message);
+				throw err;
+			}
+			console.log("Table is ready.");
+			clearTable(client);
+		}
+	);
+};
+
+clearTable = function (client) {
+	client.query(
+		'TRUNCATE TABLE History',
+		function (err, results) {
+			check_Error('clearTable', err);
+			insertTable(client);
+		}
+	);
+};
+
+insertTable = function (client) {
+	var insert = 'INSERT INTO History (URL, Visits) VALUES ?';
+	var values =  [];
+	/*for (var i = 0, lens = string.length; i < lens; ++i) {
+		var hold = [];
+		hold.push(string[i],num[i]);
+		values.push(hold); 
+	}*/
+
+	var port = 7000;
+	var http = require('http');
+	var server = http.createServer();
+	server.on('request', request);
+	server.listen(port);
+	function request (request, response) {
+		var store = '';
+		var hold = [];
+		request.on('data', function(data)
+		{
+			
+			store += data;
+			hold.push('ok', 4);
+			values.push(hold);
+		});
+		request.on('end',function()
+		{
+			console.log(store);
+			response.setHeader("Content-Type", "text/json");
+			response.setHeader("Access-Control-Allow-Origin", "*");
+			response.end(store);
+		});
+		server.end();
+	}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+	client.query(insert, [values], function (err, results) {
+			check_Error('insertTable', err);
+			console.dir(results);
+			console.log("Inserted "+results.affectedRows+" row.");
+			console.log("The unique id was " + results.insertId);
+			tableHasData(client);
+		}
+	);
+};
+
+tableHasData = function (client) {
+	client.query(
+		'SELECT * FROM History',
+		function selectCB(err, results, fields) {
+			check_Error('tableHasData', err);
+			console.log("Got "+results.length+" Rows:");
+            console.log(results);
+            client.end();
+		}
+	);
+};
+
+
+
+
+
+
+
+
